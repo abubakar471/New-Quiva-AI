0024dba83be6c78b34c2964803b9a335b68d31a0
Alto041 <98416387+Alto041@users.noreply.github.com>
2024-08-02 23:31:37 +0530

    Update page.jsx

--- app/getstarted/page.jsx
+++ app/getstarted/page.jsx
@@ -10,8 +10,8 @@ const BuyNowPage = () => {
             </div>
             <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_100px_at_10%_0px,#004d00,transparent)] xl:bg-[radial-gradient(circle_500px_at_90%_900px,#004d00,transparent)] "></div>
             <div className='pt-20'>
-                [                <div className='p-10 rounded-3xl relative flex flex-col gap-y-4 w-full md:w-[600px] bg-transparent shadow-lg shadow-green-500'>
-                    ]                    <div className='w-full flex items-center justify-center'>
+                                <div className='p-10 rounded-3xl relative flex flex-col gap-y-4 w-full md:w-[600px] bg-transparent shadow-lg shadow-green-500'>
+                                        <div className='w-full flex items-center justify-center'>
                         <div className='rounded-xl bg-green-500/5 text-[12px] text-white backdrop-blur-sm border border-white/10 hover:bg-white/10 hover:backdrop-blur-md transition-all p-2 shadow-xl shadow-green-500/10 w-fit'>
                             <Image src={`/assets/images/logo.png`} alt="Quiva AI" width={40} height={40} />
                         </div>
